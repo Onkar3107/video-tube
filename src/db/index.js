4f25e67acf5d688a12b3ce703c0602f6f70bf05b
Onkar Rane <onkarrane60@gmail.com>
2025-02-03 13:33:00 +0530

    Connected to Mongo-atlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
+    console.log(connectionInstance);
+    console.log(
+      `MongoDB connected!! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.error("Error: ", err);
+    process.exit(1);
+  }
+};
