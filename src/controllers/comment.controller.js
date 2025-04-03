de6f03a3bde2c532644bb8d9474c77e618d87311
Onkar Rane <onkarrane60@gmail.com>
2025-04-03 15:12:05 +0530

    Implement aggregation to retrieve video comments with pagination and user details

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,10 +8,76 @@ const getVideoComments = AsyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID.");
+  }
+
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $unwind: "$owner",
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        owner: {
+          _id: "$owner._id",
+          username: "$owner.username",
+          avatar: "$owner.avatar",
+        },
+      },
+    },
+  ])
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .sort({ createdAt: -1 });
+
+  const totalComments = await Comment.countDocuments({
+    video: new mongoose.Types.ObjectId(videoId),
+  });
+
+  const totalPages = Math.ceil(totalComments / limit);
+  const hasNextPage = page < totalPages;
+  const hasPrevPage = page > 1;
+  const nextPage = hasNextPage ? parseInt(page) + 1 : null;
+  const prevPage = hasPrevPage ? parseInt(page) - 1 : null;
+
+  const pagination = {
+    totalComments,
+    totalPages,
+    hasNextPage,
+    hasPrevPage,
+    nextPage,
+    prevPage,
+  };
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { comments, pagination },
+        "Comments retrieved successfully."
+      )
+    );
 });
 
 const addComment = AsyncHandler(async (req, res) => {
-
   const { videoId } = req.params;
   const comment = req.body.comment?.trim();
 
@@ -45,7 +111,6 @@ const addComment = AsyncHandler(async (req, res) => {
 });
 
 const updateComment = AsyncHandler(async (req, res) => {
-
   const { commentId } = req.params;
   const comment = req.body.comment?.trim();
 
@@ -83,7 +148,6 @@ const updateComment = AsyncHandler(async (req, res) => {
 });
 
 const deleteComment = AsyncHandler(async (req, res) => {
-
   const { commentId } = req.params;
 
   if (!isValidObjectId(commentId)) {
